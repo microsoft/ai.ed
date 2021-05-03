d2509e87b24dfef43f310c2bdccf5fdd7ca725dc
Titus Barik <titus.barik@microsoft.com>
2021-05-03 15:46:37 -0700

    Add prettier and prettier:fix targets.
    Reformat existing ts files in src.

--- ai-compile/vscode-extension/src/types.ts
+++ ai-compile/vscode-extension/src/types.ts
@@ -1,22 +1,19 @@
-
 export interface Payload {
-    source: string;
-    lastEditLine: number;
+  source: string;
+  lastEditLine: number;
 }
 
 export interface Response {
-    lineNo: number;
-    repairLine: string;
-    feedbacks: string;
-    repairClasses: string;
+  lineNo: number;
+  repairLine: string;
+  feedbacks: string;
+  repairClasses: string;
 }
 
 export interface DocumentStore {
-    // TODO: Can this be made private? Does it make sense?
-    filePath: string,
-    fixes: Fix    
+  // TODO: Can this be made private? Does it make sense?
+  filePath: string;
+  fixes: Fix;
 }
 
-
-export type Fix = ( Response[] | undefined );
-
+export type Fix = Response[] | undefined;
