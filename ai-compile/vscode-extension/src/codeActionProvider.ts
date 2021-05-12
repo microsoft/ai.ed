e319b3bf139eec61fcd0f747b5d74b5e48138d2f
Titus Barik <titus.barik@microsoft.com>
2021-05-11 19:23:50 -0700

    Update codeActionProvider.

--- ai-compile/vscode-extension/src/codeActionProvider.ts
+++ ai-compile/vscode-extension/src/codeActionProvider.ts
@@ -1,9 +1,65 @@
 import * as vscode from "vscode";
+import * as path from "path";
 
-export const diagosticCollection = vscode.languages.createDiagnosticCollection(
+import * as pymacer from "./pymacer";
+
+export let diagnosticCollection = vscode.languages.createDiagnosticCollection(
   "pythonedu"
 );
 
-export function updateDiagnostics(document: vscode.TextDocument) {
-    
+export function updateDiagnostics(
+  document: vscode.TextDocument,
+  fixes: pymacer.Fixes
+) {
+  if (document && path.basename(document.uri.fsPath) === "rainfall.py") {
+    diagnosticCollection.set(document.uri, [
+      {
+        code: "PYEDU_MACER",
+        message:
+          "🤗 Python couldn't understand your program.\n\n" +
+          "PUT ERROR MESSAGE HERE",
+        range: new vscode.Range(
+          new vscode.Position(6, 12),
+          new vscode.Position(6, 27)
+        ),
+        severity: vscode.DiagnosticSeverity.Warning,
+        source: "PyEdu 🐍",
+      },
+    ]);
+  } else {
+    diagnosticCollection.clear();
+  }
+}
+
+export class EduCodeActionProvider implements vscode.CodeActionProvider {
+  public static readonly providedCodeActionKinds = [
+    vscode.CodeActionKind.QuickFix,
+  ];
+
+  private createFix(diagnostic: vscode.Diagnostic): vscode.CodeAction {
+    const action = new vscode.CodeAction(
+      "Replace x with y",
+      vscode.CodeActionKind.QuickFix
+    );
+
+    action.diagnostics = [diagnostic];
+    action.isPreferred = true;
+
+    return action;
+  }
+
+  provideCodeActions(
+    document: vscode.TextDocument,
+    range: vscode.Range | vscode.Selection,
+    context: vscode.CodeActionContext,
+    token: vscode.CancellationToken
+  ): vscode.CodeAction[] {
+    for (let x of context.diagnostics) {
+      console.log(x);
+    }
+
+    return context.diagnostics
+      .filter((diagnostic) => diagnostic.code === "PYEDU_MACER")
+      .map((diagnostic) => this.createFix(diagnostic));
+  }
 }
